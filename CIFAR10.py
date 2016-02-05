ae28c6fdfcb237825206ddc96f6ea5572259310c
ad2512 <ad2512@ic.ac.uk>
2016-02-05 11:39:15 +0000

    a

--- CIFAR10.py
+++ CIFAR10.py
@@ -29,23 +29,22 @@ labels_2 = np_utils.to_categorical(labels_2)
 
 # Building Model
 model = Sequential()
-model.add(Convolution2D(17,3,3,init='uniform',border_mode='same',input_shape=(3,32,32)))
+model.add(Convolution2D(32,3,3,init='uniform',border_mode='full',input_shape=(3,32,32)))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Activation('relu'))
-model.add(Convolution2D(17, 3, 3))
+model.add(Convolution2D(32, 3, 3))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
-#model.add(Convolution2D(30, 3, 3, border_mode='same'))
+#model.add(Convolution2D(64, 3, 3, border_mode='full'))
 #model.add(Activation('relu'))
-#model.add(Convolution2D(30, 3, 3))
+#model.add(Convolution2D(64, 3, 3))
 #model.add(Activation('relu'))
 #model.add(MaxPooling2D(pool_size=(2, 2)))
 #model.add(Dropout(0.25))
 model.add(Flatten())
-model.add(Dense(100))
+model.add(Dense(200))
 model.add(Activation('relu'))
-model.add(Dropout(0.5))
 model.add(Dense(10))
 model.add(Activation('softmax'))
 
@@ -53,7 +52,7 @@ model.add(Activation('softmax'))
 sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
-model.fit(train, labels_1, batch_size=200, nb_epoch=20,verbose=1,show_accuracy=True,validation_data=(test, labels_2))
+model.fit(train, labels_1, batch_size=300, nb_epoch=20,verbose=1,show_accuracy=True,validation_data=(test, labels_2))
 
 
 
