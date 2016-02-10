e325e84c6d73416bf859f11c3a6b894efa599128
ad2512 <ad2512@ic.ac.uk>
2016-02-10 18:10:49 +0000

    a

--- MNIST.py
+++ MNIST.py
@@ -40,10 +40,11 @@ model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten())
 model.add(Dense(100))
+model.add(Dropout(0.2))
 model.add(Dense(10))
 model.add(Activation('softmax'))
 
-rms = RMSprop(lr=0.001, rho=0.9, epsilon=1e-06)
+rms = RMSprop(lr=0.001, rho=0.9, epsilon=1e-10)
 model.compile(loss='categorical_crossentropy', optimizer=rms)
 
 model.fit(train, labels_1, batch_size=200, nb_epoch=20,verbose=1,show_accuracy=True,validation_data=(test, labels_2))
