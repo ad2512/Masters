6925c356fa6b2f242834368625d3d257c2a55f1a
Adam C R Durrant <ad2512@maws414-060.ma.ic.ac.uk>
2016-02-11 12:22:52 +0000

    a

--- MNIST.py
+++ MNIST.py
@@ -40,7 +40,7 @@ model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten())
 model.add(Dense(100))
-model.add(Dropout(0.1))
+model.add(Dropout(0.2))
 model.add(Dense(10))
 model.add(Activation('softmax'))
 
