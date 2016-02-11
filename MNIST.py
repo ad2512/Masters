f8b8b2c892ff7593fabb236fc9771d1a90c41e1d
Adam C R Durrant <ad2512@maws414-060.ma.ic.ac.uk>
2016-02-11 12:26:55 +0000

    a

--- MNIST.py
+++ MNIST.py
@@ -39,8 +39,8 @@ model.add(Convolution2D(15,2,2))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten())
-model.add(Dense(100))
 model.add(Dropout(0.2))
+model.add(Dense(100))
 model.add(Dense(10))
 model.add(Activation('softmax'))
 
@@ -48,7 +48,7 @@ sgd = SGD(lr=0.003, decay=0.0005,nestorov=True)
 #rms = RMSprop(lr=0.001, rho=0.95, epsilon=1e-15)
 model.compile(loss='categorical_crossentropy', optimizer=sgd)
 
-model.fit(train, labels_1, batch_size=300, nb_epoch=100,verbose=1,show_accuracy=True,validation_data=(test, labels_2))
+model.fit(train, labels_1, batch_size=300, nb_epoch=30,verbose=1,show_accuracy=True,validation_data=(test, labels_2))
 
 
 print "Dropout = 0.4"
@@ -61,8 +61,8 @@ model.add(Convolution2D(15,2,2))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten())
-model.add(Dense(100))
 model.add(Dropout(0.4))
+model.add(Dense(100))
 model.add(Dense(10))
 model.add(Activation('softmax'))
 
@@ -70,7 +70,7 @@ sgd = SGD(lr=0.003, decay=0.0005,nestorov=True)
 #rms = RMSprop(lr=0.001, rho=0.95, epsilon=1e-15)
 model.compile(loss='categorical_crossentropy', optimizer=sgd)
 
-model.fit(train, labels_1, batch_size=300, nb_epoch=100,verbose=1,show_accuracy=True,validation_data=(test, labels_2))
+model.fit(train, labels_1, batch_size=300, nb_epoch=30,verbose=1,show_accuracy=True,validation_data=(test, labels_2))
 
 
 print "Dropout = 0.6"
@@ -83,8 +83,8 @@ model.add(Convolution2D(15,2,2))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten())
-model.add(Dense(100))
 model.add(Dropout(0.6))
+model.add(Dense(100))
 model.add(Dense(10))
 model.add(Activation('softmax'))
 
@@ -92,7 +92,7 @@ sgd = SGD(lr=0.003, decay=0.0005,nestorov=True)
 #rms = RMSprop(lr=0.001, rho=0.95, epsilon=1e-15)
 model.compile(loss='categorical_crossentropy', optimizer=sgd)
 
-model.fit(train, labels_1, batch_size=300, nb_epoch=100,verbose=1,show_accuracy=True,validation_data=(test, labels_2))
+model.fit(train, labels_1, batch_size=300, nb_epoch=30,verbose=1,show_accuracy=True,validation_data=(test, labels_2))
 
 
 
@@ -106,8 +106,8 @@ model.add(Convolution2D(15,2,2))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten())
-model.add(Dense(100))
 model.add(Dropout(0.8))
+model.add(Dense(100))
 model.add(Dense(10))
 model.add(Activation('softmax'))
 
@@ -115,7 +115,7 @@ sgd = SGD(lr=0.003, decay=0.0005,nestorov=True)
 #rms = RMSprop(lr=0.001, rho=0.95, epsilon=1e-15)
 model.compile(loss='categorical_crossentropy', optimizer=sgd)
 
-model.fit(train, labels_1, batch_size=300, nb_epoch=100,verbose=1,show_accuracy=True,validation_data=(test, labels_2))
+model.fit(train, labels_1, batch_size=300, nb_epoch=30,verbose=1,show_accuracy=True,validation_data=(test, labels_2))
 
 
 
