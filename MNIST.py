91288f71b8958af0c8974b49272490758535bcb6
ad2512 <ad2512@ic.ac.uk>
2016-02-29 15:00:55 +0000

    a

--- MNIST.py
+++ MNIST.py
@@ -54,7 +54,7 @@ labels_2 = np_utils.to_categorical(labels_2)
 
 model = Sequential()
 model.add(Convolution2D(16,2,2,init='uniform',border_mode='valid',input_shape=(1,28,28)))
-model.add(Activation('sigmoid'))
+model.add(Activation('ReLU'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten())
 model.add(Dropout(0.25))
