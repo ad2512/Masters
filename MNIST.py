945acf2d1ad8e564e19e520dd249381e64a6100d
ad2512 <ad2512@ic.ac.uk>
2016-02-29 22:29:51 +0000

    a

--- MNIST.py
+++ MNIST.py
@@ -57,7 +57,7 @@ model.add(Convolution2D(16,2,2,init='uniform',border_mode='valid',input_shape=(1
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten())
-model.add(Dropout(0.2))
+model.add(Dropout(0.4))
 model.add(Dense(10))
 model.add(Activation('softmax'))
 
