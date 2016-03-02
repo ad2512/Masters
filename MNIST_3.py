b558766a239cf939416fc9e9b393c27d6076d3fc
ad2512 <ad2512@ic.ac.uk>
2016-03-02 19:07:18 +0000

    a

--- MNIST_3.py
+++ MNIST_3.py
@@ -53,7 +53,7 @@ model1.add(Dropout(0.5))
 model1.add(Dense(10))
 model1.add(Activation('softmax'))
 model1.compile(loss='categorical_crossentropy', optimizer="RMSprop")
-model1.fit(train, labels_1, batch_size=300, nb_epoch=1,verbose=1,show_accuracy=True,validation_data=(test, labels_2a))
+model1.fit(train, labels_1, batch_size=300, nb_epoch=nb,verbose=1,show_accuracy=True,validation_data=(test, labels_2a))
 classes1 = model1.predict_classes(test, batch_size=300)
 
 # Building Model - Model 1
@@ -75,7 +75,7 @@ model2.add(Dropout(0.5))
 model2.add(Dense(10))
 model2.add(Activation('softmax'))
 model2.compile(loss='categorical_crossentropy', optimizer="RMSprop")
-model2.fit(train, labels_1, batch_size=300, nb_epoch=1,verbose=1,show_accuracy=True,validation_data=(test, labels_2a))
+model2.fit(train, labels_1, batch_size=300, nb_epoch=nb,verbose=1,show_accuracy=True,validation_data=(test, labels_2a))
 classes2 = model2.predict_classes(test, batch_size=300)
 
 # Building Model - Model 1
@@ -97,7 +97,7 @@ model3.add(Dropout(0.5))
 model3.add(Dense(10))
 model3.add(Activation('softmax'))
 model3.compile(loss='categorical_crossentropy', optimizer="RMSprop")
-model3.fit(train, labels_1, batch_size=300, nb_epoch=1,verbose=1,show_accuracy=True,validation_data=(test, labels_2a))
+model3.fit(train, labels_1, batch_size=300, nb_epoch=nb,verbose=1,show_accuracy=True,validation_data=(test, labels_2a))
 classes3 = model3.predict_classes(test, batch_size=300)
 
 # Building Model - Model 1
@@ -119,7 +119,7 @@ model4.add(Dropout(0.5))
 model4.add(Dense(10))
 model4.add(Activation('softmax'))
 model4.compile(loss='categorical_crossentropy', optimizer="RMSprop")
-model4.fit(train, labels_1, batch_size=300, nb_epoch=1,verbose=1,show_accuracy=True,validation_data=(test, labels_2a))
+model4.fit(train, labels_1, batch_size=300, nb_epoch=nb,verbose=1,show_accuracy=True,validation_data=(test, labels_2a))
 classes4 = model4.predict_classes(test, batch_size=300)
 
 # Building Model - Model 1
@@ -141,7 +141,7 @@ model5.add(Dropout(0.5))
 model5.add(Dense(10))
 model5.add(Activation('softmax'))
 model5.compile(loss='categorical_crossentropy', optimizer="RMSprop")
-model5.fit(train, labels_1, batch_size=300, nb_epoch=1,verbose=1,show_accuracy=True,validation_data=(test, labels_2a))
+model5.fit(train, labels_1, batch_size=300, nb_epoch=nb,verbose=1,show_accuracy=True,validation_data=(test, labels_2a))
 classes5 = model5.predict_classes(test, batch_size=300)
 
 # Calculating modes
