2430abcdcd4cdca9612737f9f3ca7b2acfcae496
ad2512 <ad2512@ic.ac.uk>
2016-03-17 14:22:49 +0000

    A

---
+++ Cancer.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import cPickle
+import gzip
+import numpy as np
+import theano
+import theano.tensor as T
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.datasets import mnist
+from keras.optimizers import SGD, RMSprop
+from keras.utils import np_utils, generic_utils
+from theano.tensor.nnet import conv
+from theano.tensor.nnet import softmax
+from theano.tensor import shared_randomstreams
+from theano.tensor.signal import downsample
+from theano.tensor.nnet import sigmoid
+from theano.tensor import tanh
+import pylab as pl
+import matplotlib.cm as cm
+import os, struct
+from array import array as pyarray
+from numpy import append, array, int8, uint8, zeros
+from matplotlib.pyplot import imshow
