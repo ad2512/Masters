f5ee356da3e1e2f1573ec84fc264f9742054b8fd
ad2512 <ad2512@ic.ac.uk>
2016-03-17 14:59:20 +0000

    A

--- Cancer.py
+++ Cancer.py
@@ -21,5 +21,27 @@ import pylab as pl
 import matplotlib.cm as cm
 import os, struct
 from array import array as pyarray
-from numpy import append, array, int8, uint8, zeros
+from numpy import append, array, int8, uint8, zeros,genfromtxt
 from matplotlib.pyplot import imshow
+from sklearn.cross_validation import train_test_split
+
+
+data = genfromtxt("N" + str(i+1) + ".csv",delimiter=',')
+s = np.shape(data)[0]
+all_data=[]
+for i in range(696):
+	data = genfromtxt("N" + str(i+1) + ".csv",delimiter=',')
+	a = data[0:s,0:s]
+	b = data[0:s,s:(2*s)]
+	c = data[0:s,(2*s):(3*s)]
+	d = np.dstack((a,b,c))
+	all_data = all_data.append(d)
+	
+train, test, labels_1, labels_2 = train_test_split(all_data,labels,test_size=0.2)
+train = train.reshape(np.shape(train)[0],3,s,s)
+train = train.astype('float32')
+test_r=test
+test = test.reshape(np.shape(test)[0],3,s,s)
+test = test.astype('float32')
+labels_1 = np_utils.to_categorical(labels_1)
+labels_2a = np_utils.to_categorical(labels_2)
