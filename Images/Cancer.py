3450faeb23d92511aa925306e0a06a41a4bab1cc
ad2512 <ad2512@ic.ac.uk>
2016-03-18 12:36:22 +0000

    A

--- Images/Cancer.py
+++ Images/Cancer.py
@@ -26,6 +26,7 @@ from matplotlib.pyplot import imshow
 from sklearn.cross_validation import train_test_split
 from random import randint
 
+# Setting up the Data
 data = genfromtxt("N" + str(1) + ".csv",delimiter=',')
 s = np.shape(data)[0]
 a = data[0:s,0:s]
@@ -38,18 +39,18 @@ b = data[0:s,s:(2*s)]
 c = data[0:s,(2*s):(3*s)]
 d1 = np.dstack((a,b,c))
 all_data=[d,d1]
-labels=[0,1]
-for i in range(694):
+labels=genfromtxt("Labels.csv",delimiter=',')
+for i in range(np.size(labels)-3):
+	print(i+3)
 	data = genfromtxt("N" + str(i+3) + ".csv",delimiter=',')
 	a = data[0:s,0:s]
 	b = data[0:s,s:(2*s)]
 	c = data[0:s,(2*s):(3*s)]
 	d = np.dstack((a,b,c))
 	all_data.append(d)
-	labels.append(randint(0,1))
-	print(i)
 	
-train, test, labels_1, labels_2 = train_test_split(all_data,labels,test_size=0.2)
+	
+train, test, labels_1, labels_2 = train_test_split(all_data,labels,test_size=0.4)
 train = train.reshape(np.shape(train)[0],3,s,s)
 train = train.astype('float32')
 test_r=test
@@ -57,3 +58,27 @@ test = test.reshape(np.shape(test)[0],3,s,s)
 test = test.astype('float32')
 labels_1 = np_utils.to_categorical(labels_1)
 labels_2a = np_utils.to_categorical(labels_2)
+
+# Building Model
+model = Sequential()
+model.add(Convolution2D(8,5,5,init='uniform',border_mode='full',input_shape=(3,s,s)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+#model.add(Convolution2D(32, 5, 5, border_mode='full'))
+#model.add(Activation('relu'))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
+#model.add(Dropout(0.25))
+#model.add(Convolution2D(64, 5, 5, border_mode='full'))
+#model.add(Activation('relu'))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
+#model.add(Dropout(0.5))
+model.add(Flatten())
+model.add(Dense(20))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer="RMSprop")
+model.fit(train, labels_1, batch_size=50, nb_epoch=10,verbose=1,show_accuracy=True,validation_data=(test, labels_2a))
