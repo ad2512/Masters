251c7153da9bf760c618146594cd9bf8d74adcbf
ad2512 <ad2512@ic.ac.uk>
2016-03-21 15:17:08 +0000

    all

--- Histo6.py
+++ Histo6.py
@@ -76,7 +76,7 @@ model.add(Flatten())
 model.add(Dense(6))
 model.add(Activation('softmax'))
 
-theano.shared(model.compile(loss='categorical_crossentropy', optimizer="RMSprop"),borrow=True)
+model.compile(loss='categorical_crossentropy', optimizer="RMSprop")
 model.fit(all_data[0:10], labels[0:10], batch_size=1, nb_epoch=1,verbose=1,show_accuracy=True,validation_data=(all_data[400:539], labels[400:539]))
 
 
