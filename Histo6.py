c52d9bc1493df54a2a8733c7209c8aac646630ea
ad2512 <ad2512@ic.ac.uk>
2016-03-22 17:25:13 +0000

    a

--- Histo6.py
+++ Histo6.py
@@ -84,6 +84,6 @@ model.add(Dropout(0.25))
 model.add(Dense(6))
 model.add(Activation('softmax'))
 
-sgd = SGD(lr=0.000001, decay=1e-6, momentum=0.9, nesterov=True)
+sgd = SGD(lr=0.0000001, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='mse', optimizer=sgd)
-model.fit(all_data[150:210], labels[150:210], batch_size=3, nb_epoch=20,verbose=1,show_accuracy=True,validation_data=(all_data[400:539], labels[400:539]))
+model.fit(all_data, labels, batch_size=3, nb_epoch=50,verbose=1,show_accuracy=True,validation_data=(all_data[300:539], labels[300:539]))
