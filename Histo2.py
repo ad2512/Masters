fcd90630ab392dd6d5a14087b9493858a0b7d863
ad2512 <ad2512@ic.ac.uk>
2016-03-27 18:51:41 +0100

    a

--- Histo2.py
+++ Histo2.py
@@ -90,7 +90,7 @@ model.add(Dense(2))
 model.add(Activation('softmax'))
 
 sgd = SGD(lr=0.000001, decay=1e-6, momentum=0.9, nesterov=True)
-RMS = RMSprop(lr=0.000000002, rho=0.7, epsilon=1e-08)
+RMS = RMSprop(lr=0.0000000005, rho=0.7, epsilon=1e-08)
 model.compile(loss='categorical_crossentropy', optimizer=RMS)
 model.fit(all_data[0:400], labels[0:400], batch_size=5, nb_epoch=200,verbose=1,show_accuracy=True,validation_data=(all_data[400:488], labels[400:488]))
 
