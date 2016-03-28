c5bb30cd608789ed59b1e4a55a9eaf6d9ce3c559
ad2512 <ad2512@ic.ac.uk>
2016-03-28 18:07:06 +0100

    a

--- FIDO.py
+++ FIDO.py
@@ -9,7 +9,7 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.datasets import mnist
-from keras.optimizers import SGD, RMSprop
+from keras.optimizers import SGD, RMSprop, Adadelta
 from keras.utils import np_utils, generic_utils
 from theano.tensor.nnet import conv
 from theano.tensor.nnet import softmax
@@ -92,5 +92,6 @@ model.add(Activation('softmax'))
 
 sgd = SGD(lr=0.000001, decay=1e-6, momentum=0.9, nesterov=True)
 RMS = RMSprop(lr=0.0000000005, rho=0.7, epsilon=1e-08)
-model.compile(loss='categorical_crossentropy', optimizer=RMS)
+Ada = Adadelta(lr=0.001, rho=0.95, epsilon=1e-06)
+model.compile(loss='categorical_crossentropy', optimizer=Ada)
 model.fit(all_data[0:550], labels[0:550], batch_size=5, nb_epoch=1000,verbose=1,show_accuracy=True,validation_data=(all_data[550:662], labels[550:662]))
