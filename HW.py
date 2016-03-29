7e3ba197f429ba09449f0b0f8d149e8839863ab2
ad2512 <ad2512@ic.ac.uk>
2016-03-29 02:20:57 +0100

    a

---
+++ HW.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import simps
+import compiler
+
+# Define 'a' and 'b' and number of grid points (the more the better)
+
+a = float(raw_input("Please enter your value of 'a': "))
+b = float(raw_input("Please enter your value of 'b': "))
+g = float(raw_input("Please enter the number of gridpoints: "))
+x = np.linspace(a,b,num=g)
+
+# Define your 'y' function
+
+f = raw_input("Please enter your equation 'y': ")
+y = eval(f)
+
+# Integration section
+
+x1 = simps(x*y,x)
+d = simps(y,x)
+y1 = simps(y*y,x)
+x_bar = x1/d
+y_bar = 0.5*y1/d
+
+# Plotting curve
+
+plt.figure() #make new figure
+plt.plot(x,y)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Plot of your curve')
+plt.savefig('Curve.png')	
