62edc019b41797dedcb5d24154fecd186db73784
ad2512 <ad2512@ic.ac.uk>
2016-04-03 23:25:37 +0100

    a

--- Histo6R.py
+++ Histo6R.py
@@ -9,7 +9,7 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.datasets import mnist
-from keras.optimizers import SGD, RMSprop
+from keras.optimizers import SGD, RMSprop, Adadelta
 from keras.utils import np_utils, generic_utils
 from theano.tensor.nnet import conv
 from theano.tensor.nnet import softmax
@@ -26,14 +26,15 @@ from matplotlib.pyplot import imshow
 from sklearn.cross_validation import train_test_split
 from random import randint
 import cv2
+from collections import Counter
+import random
 
 # Setting up the Data
-A=539;
-B=144;
-l = float(genfromtxt("/home/silo1/ad2512/Histo_6/L" + str(1) + ".csv",delimiter=','))
-l1 = float(genfromtxt("/home/silo1/ad2512/Histo_6/L" + str(2) + ".csv",delimiter=','))
-d = cv2.imread('/home/silo1/ad2512/Histo_6/255NORM1.jpg')
-d1 = cv2.imread('/home/silo1/ad2512/Histo_6/255NORM2.jpg')
+A=144;
+l = float(genfromtxt("/home/silo1/ad2512/Histo_6/LReduced" + str(1) + ".csv",delimiter=','))
+l1 = float(genfromtxt("/home/silo1/ad2512/Histo_6/LReduced" + str(2) + ".csv",delimiter=','))
+d = cv2.imread('/home/silo1/ad2512/Histo_6/2Reduced1.jpg')
+d1 = cv2.imread('/home/silo1/ad2512/Histo_6/Reduced2.jpg')
 all_data=[d,d1]
 labels=[l,l1]
 for i in range(A-2):
@@ -44,66 +45,118 @@ for i in range(A-2):
 	all_data.append(d)
 	labels.append(l)
 
-l = float(genfromtxt("/home/silo1/ad2512/Histo_6/LReduced" + str(1) + ".csv",delimiter=','))
-l1 = float(genfromtxt("/home/silo1/ad2512/Histo_6/LReduced" + str(2) + ".csv",delimiter=','))
-d = cv2.imread('/home/silo1/ad2512/Histo_6/Reduced1.jpg')
-d1 = cv2.imread('/home/silo1/ad2512/Histo_6/Reduced2.jpg')
-train=[d,d1]
-train_labels=[l,l1]
-for i in range(B-2):
-	if((i+3)>A):
-		break
-	l = float(genfromtxt("/home/silo1/ad2512/Histo_6/LReduced" + str(i+3) + ".csv",delimiter=','))
-	d = cv2.imread("/home/silo1/ad2512/Histo_6/Reduced" + str(i+3) + ".jpg")
-	train.append(d)
-	train_labels.append(l)
-	
 s = np.shape(all_data)[1]
 all_data = np.asarray(all_data)	
 all_data = all_data.astype('float32')
 all_data = all_data.reshape(A,3,s,s)
+all_data /= np.max(np.abs(all_data),axis=0)
 labels = np.asarray(labels)
 labels = labels.astype('int')
+nb_classes = np.size(np.unique(labels))
+prop = 0.8;
+train_labels=[]
+train_data=[]
+test_labels=[]
+test_data=[]
+c = Counter(labels)
+def scrambled(orig):
+    dest = orig[:]
+    random.shuffle(dest)
+    return dest
+
+for i in range(np.size(np.unique(labels))):
+	a=[];
+	b=[];
+	for j in range(np.size(labels)):
+		if(np.size(a)<prop*c[i]):
+			if(labels[j]==i):
+				a.extend([j])
+		else:
+			if(labels[j]==i):
+				b.extend([j])
+	train_labels.extend(labels[a])	
+	train_data.extend(all_data[a])
+	test_labels.extend(labels[b])
+	test_data.extend(all_data[b])
+
+a = range(np.size(train_labels))
+b = scrambled(a)
+train_data = [train_data[i] for i in b]
+train_data = np.asarray(train_data)
+test_data = np.asarray(test_data)
+train_labels = [train_labels[i] for i in b]
 labels = np_utils.to_categorical(labels)
-train = np.asarray(train)	
-train = train.astype('float32')
-train = train.reshape(B,3,s,s)
-train_labels = np.asarray(train_labels)
-train_labels = train_labels.astype('int')
+c = Counter(train_labels)
+print(c)
+c = Counter(test_labels)
+print(c)
 train_labels = np_utils.to_categorical(train_labels)
+test_labels = np_utils.to_categorical(test_labels)
 
 # Building Model
 # Building Model
 model = Sequential()
-model.add(Convolution2D(16,3,3,init='uniform',border_mode='full',input_shape=(3,s,s)))
-model.add(Activation('relu'))
-#model.add(Convolution2D(16, 3, 3))
-#model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(3, 2)))
-model.add(Dropout(0.25))
-model.add(Convolution2D(32,3,3,init='uniform',border_mode='full',input_shape=(3,s,s)))
-model.add(Activation('relu'))
-#model.add(Convolution2D(32, 3, 3))
-#model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(3, 2)))
-model.add(Dropout(0.25))
-model.add(Convolution2D(64, 3, 3, border_mode='full'))
-#model.add(Activation('relu'))
-#model.add(Convolution2D(64, 3, 3))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(3, 2)))
-model.add(Dropout(0.25))
+model.add(Convolution2D(12,3,3,init='uniform',border_mode='full',input_shape=(3,s,s)))
+model.add(Activation('tanh'))
+model.add(Convolution2D(12, 3, 3))
+model.add(Activation('tanh'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+model.add(Convolution2D(24,3,3,border_mode='full'))
+model.add(Activation('tanh'))
+model.add(Convolution2D(24, 3, 3))
+model.add(Activation('tanh'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+#model.add(Dropout(0.5))
+#model.add(Convolution2D(48, 3, 3, border_mode='full'))
+#model.add(Activation('tanh'))
+#model.add(Convolution2D(48, 3, 3))
+#model.add(Activation('tanh'))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
-model.add(Dense(1000))
-model.add(Activation('relu'))
-#model.add(Dropout(0.25))
-#model.add(Dense(500))
-#model.add(Activation('relu'))
-model.add(Dropout(0.25))
-model.add(Dense(6))
+model.add(Dropout(0.5))
+model.add(Dense(100))
+model.add(Activation('tanh'))
+model.add(Dropout(0.5))
+model.add(Dense(nb_classes))
 model.add(Activation('softmax'))
 
-#sgd = SGD(lr=0.0000001, decay=1e-6, momentum=0.9, nesterov=True)
-RMS = RMSprop(lr=0.00000001, rho=0.8, epsilon=1e-06)
-model.compile(loss='categorical_crossentropy', optimizer=RMS)
-model.fit(train, train_labels, batch_size=5, nb_epoch=100,verbose=1,show_accuracy=True,validation_data=(all_data, labels))
+sgd = SGD(lr=0.000001, decay=1e-6, momentum=0.9, nesterov=True)
+RMS = RMSprop(lr=0.0000000005, rho=0.7, epsilon=1e-08)
+Ada = Adadelta(lr=0.00001, rho=0.95, epsilon=1e-08)
+model.compile(loss='categorical_crossentropy', optimizer=Ada)
+model.fit(train_data, train_labels, batch_size=8, nb_epoch=2000,verbose=1,show_accuracy=True,validation_data=(test_data, test_labels))
+
+# Building Model
+# Building Model
+model = Sequential()
+model.add(Convolution2D(12,3,3,init='uniform',border_mode='full',input_shape=(3,s,s)))
+model.add(Activation('tanh'))
+model.add(Convolution2D(12, 3, 3))
+model.add(Activation('tanh'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+model.add(Convolution2D(24,3,3,border_mode='full'))
+model.add(Activation('tanh'))
+model.add(Convolution2D(24, 3, 3))
+model.add(Activation('tanh'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+#model.add(Dropout(0.5))
+#model.add(Convolution2D(48, 3, 3, border_mode='full'))
+#model.add(Activation('tanh'))
+#model.add(Convolution2D(48, 3, 3))
+#model.add(Activation('tanh'))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dropout(0.5))
+model.add(Dense(100))
+model.add(Activation('tanh'))
+model.add(Dropout(0.5))
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
+
+sgd = SGD(lr=0.000001, decay=1e-6, momentum=0.9, nesterov=True)
+RMS = RMSprop(lr=0.0000000005, rho=0.7, epsilon=1e-08)
+Ada = Adadelta(lr=0.001, rho=0.95, epsilon=1e-06)
+model.compile(loss='categorical_crossentropy', optimizer = RMS)
+model.fit(train_data, train_labels, batch_size=8, nb_epoch=200,verbose=1,show_accuracy=True,validation_data=(test_data, test_labels))
